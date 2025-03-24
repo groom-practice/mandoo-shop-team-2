beb0512fbf8ca5fc5817c4a33a8615d1672ff4b5
Yusi <yusi4781@naver.com>
2025-03-24 13:38:04 +0900

    feat: 구매하기 기능 구현

--- order/script.js
+++ order/script.js
@@ -18,3 +18,52 @@ function order() {
 }
 
 order();
+
+let orderInfo = {
+  name: "",
+  phone: "",
+  address: "",
+  setData(key, value) {
+    console.log(key, value);
+    this[key] = value;
+  },
+};
+
+const orderName = document.getElementById("orderName");
+const orderPhoneNum = document.getElementById("orderPhoneNum");
+const orderAddress = document.getElementById("orderAddress");
+const submitOrder = document.getElementById("submitOrder");
+
+orderName.addEventListener("change", (e) =>
+  orderInfo.setData("name", e.target.value)
+);
+orderPhoneNum.addEventListener("change", (e) =>
+  orderInfo.setData("phone", e.target.value)
+);
+orderAddress.addEventListener("change", (e) =>
+  orderInfo.setData("address", e.target.value)
+);
+
+submitOrder.addEventListener("click", () => {
+  if (
+    orderInfo.name === "" ||
+    orderInfo.phone === "" ||
+    orderInfo.address === ""
+  ) {
+    alert("정보 입력을 다시 해주세요.");
+    return;
+  }
+
+  const isConfirm = confirm(`
+    name: ${orderInfo.name}\n
+    phone: ${orderInfo.phone}\n
+    address: ${orderInfo.address}\n
+    위 정보로 주문하시곘습니까?
+    `);
+
+  if (isConfirm) {
+    alert(`${orderInfo.name}님 주문이 완료되었습니다!`);
+    sessionStorage.removeItem("orderItem");
+    location.href = "/index.html";
+  } else alert(`주문 실패`);
+});

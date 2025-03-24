f0ba3179145eae3a0e4094e8053aa9297a1cd5d2
조연경 <sig.yg5057@gmail.com>
2025-03-24 14:02:20 +0900

    fix: querySelector 버그 수정

--- carts/script.js
+++ carts/script.js
@@ -1,20 +1,28 @@
-const cartContainerUl = document.querySelector('.cart-container ul');
+const cartContainerUl = document.querySelector('.cart-container');
+if (!cartContainerUl) {
+  console.error("cartContainerUl 요소를 찾을 수 없습니다. 선택자를 확인하세요.");
+}
+
 const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];   // 로컬스토리지 배열명 임의로 지정, 추 후 수정 필요
 
 // 1. 장바구니 목록 띄우기
 function renderCartItems() {
   // 장바구니에 담긴 상품이 없을때
-  if (!cartItems) {
+  if (cartItems.length === 0) {
     cartContainerUl.innerHTML = "장바구니에 담긴 상품이 없습니다.";
     return;
   }
   cartItems.forEach((item) => {
     const li = document.createElement("li");
-    li.innerHTML = `<input type="checkbox" id="${item.productName}" /><label for="${item.productName}">${item.productName}</label>`;
+    li.innerHTML = `
+      <input type="checkbox" id="${item.productName}" />
+      <label for="${item.productName}">${item.productName}</label>
+    `;
     cartContainerUl.appendChild(li);
   });
 }
 renderCartItems();
+console.log('cartItems', cartItems);
 
 const deleteButton = document.querySelector(".delete-button"); // 선택 삭제 버튼
 
